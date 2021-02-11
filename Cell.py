36590cfa29c40701c85e89d415a556bcf761bab9
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2021-02-11 15:35:41 -0500

    quick fix

--- Cell.py
+++ Cell.py
@@ -7,6 +7,8 @@ class Cell:
         # not all cells have Agents
         if Agent is not None:
             self.agent = Agent
+        else:
+            self.agent = None
 
         self.color = None
         self.concentration_capacity = None
