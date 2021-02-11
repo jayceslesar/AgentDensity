dd40533ea5202c620b07f13dd914606213a5a2b7
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2021-02-11 15:35:02 -0500

    initial cell

--- Cell.py
+++ Cell.py
@@ -1,8 +1,13 @@
-class Cell:
-    def __init__(self, row, column, Agent=None):
+import Agent
 
-        if Agent:
+
+class Cell:
+    def __init__(self, row: int, column: int, Agent=None):
+
+        # not all cells have Agents
+        if Agent is not None:
             self.agent = Agent
+
         self.color = None
         self.concentration_capacity = None
         self.diffusion_rate = None
