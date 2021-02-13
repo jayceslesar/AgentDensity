2957c9f6a251f3f563798d5d55e38a4e9b3344f1
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2021-02-13 15:26:39 -0500

    cell is done

--- Cell.py
+++ Cell.py
@@ -11,5 +11,31 @@ class Cell:
             self.agent = None
 
         self.color = None
-        self.concentration_capacity = None
+        self.concentration_capacity = 0
         self.diffusion_rate = None
+        self.gradient_map = {0.2: (249, 189, 138), 0.4: (246, 135, 86), 0.6: (248, 110, 49), 0.8: (243, 95, 30), 1.0: (251, 69, 3)}
+
+    def get_color(self):
+        if self.Agent is not None:
+            return agent.get_color()
+        else:
+            return self._color()
+
+    def _color(self):
+        if self.concentration_capacity < 0.2:
+            return self.gradient_map[0.2]
+        if self.concentration_capacity >= 0.2 and self.concentration_capacity < 0.4:
+            return self.gradient_map[0.4]
+        if self.concentration_capacity >= 0.4 and self.concentration_capacity < 0.6:
+            return self.gradient_map[0.6]
+        if self.concentration_capacity >= 0.6 and self.concentration_capacity < 0.8:
+            return self.gradient_map[0.8]
+        if self.concentration_capacity >= 0.8:
+            return self.gradient_map[1.0]
+
+    def __str__(self):
+        if self.Agent is not None:
+            return str(agent)
+        else:
+            return 'E'
+
