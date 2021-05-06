f0998e1080bd436ab6eec81611a42c9948df37aa
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2021-05-06 12:18:48 -0400

    yeah

--- room_analysis.py
+++ room_analysis.py
@@ -26,7 +26,7 @@ def last_step_auc_heatmap(csv_name):
     heatmap = [row5, row4, row3, row2, row1]
 
     fig = ff.create_annotated_heatmap(heatmap)
-    imagename = os.path.join(os.getcwd(), "data", csv_name+'_heatmap.svg')
+    imagename = os.path.join(os.getcwd(), "data", csv_name+'_E2_heatmap.svg')
     pio.write_image(fig, imagename, format='svg', width=1100, height=850)
 
 
@@ -53,33 +53,37 @@ def auc_distrs_chunk(csv_name, bin_sizes=[100, 200, 300, 400, 500, 600, 1200, 24
 
     i = 0
     for name, group in ordered_groups:
-        if int(name) % bin_sizes[i] == 0:
+        if int(name) == bin_sizes[i]:
             distrs.append(group['agent_auc'].values)
             i += 1
 
     return distrs
 
 
-# x = get_rolling_average('p11_p21_p31.csv')
-# y = get_rolling_average('p12_p21_p31.csv')
-# z = get_rolling_average('p13_p21_p31.csv')
-# t = [(i*100) + 1 for i in range(30)]
+x = get_rolling_average('p11_p21_p31.csv')
+y = get_rolling_average('p11_p22_p31.csv')
+z = get_rolling_average('p11_p23_p31.csv')
+last_step_auc_heatmap('p11_p21_p31.csv')
+last_step_auc_heatmap('p11_p22_p31.csv')
+last_step_auc_heatmap('p11_p23_p31.csv')
+t = [(i*100) + 1 for i in range(30)]
 
 
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(x=t, y=x, mode='lines', name='p11_p21_p31' + ' min', marker_color='blue'))
-# fig.add_trace(go.Scatter(x=t, y=y, mode='lines', name='p12_p21_p31' + ' max', marker_color='green'))
-# fig.add_trace(go.Scatter(x=t, y=z, mode='lines', name='p13_p21_p31' + ' mean', marker_color='red'))
-# fig.update_layout(title='AUC Different Sink/Source Locations', font_family='arial', font_size=20)
-# fig.update_xaxes(title='Step')
-# fig.update_yaxes(title='AUC')
-# imagename = os.path.join(os.getcwd(), "data", 'P1_Variation.svg')
-# pio.write_image(fig, imagename, format='svg', width=1100, height=850)
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=t, y=x, mode='lines', name='p11_p21_p31', marker_color='blue'))
+fig.add_trace(go.Scatter(x=t, y=y, mode='lines', name='p11_p22_p31', marker_color='green'))
+fig.add_trace(go.Scatter(x=t, y=z, mode='lines', name='p11_p23_p31', marker_color='red'))
+fig.update_layout(title='AUC Different Sink/Source Locations', font_family='arial', font_size=20)
+fig.update_xaxes(title='Step')
+fig.update_yaxes(title='AUC')
+fig.show()
+imagename = os.path.join(os.getcwd(), "data", 'E2_Variation.svg')
+pio.write_image(fig, imagename, format='svg', width=1100, height=850)
 
 
 x = auc_distrs_chunk('p11_p21_p31.csv')
-y = auc_distrs_chunk('p12_p21_p31.csv')
-z = auc_distrs_chunk('p13_p21_p31.csv')
+y = auc_distrs_chunk('p11_p22_p31.csv')
+z = auc_distrs_chunk('p11_p23_p31.csv')
 
 df = pd.DataFrame()
 
@@ -105,13 +109,12 @@ df['step'] = steps + steps + steps
 df['sim_group'] = x_group + y_group + z_group
 df['bins'] = all_bins + all_bins + all_bins
 
-# TODO scale x axes
-
 
 fig = px.box(df, x="bins", y="val", color="sim_group")
 fig.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
-fig.update_xaxes(title='Step')
+fig.update_xaxes(title='Seconds From Simulation Start')
 fig.update_yaxes(title='Virus Intake')
-fig.show()
+imagename = os.path.join(os.getcwd(), "data", 'E2_boxplot.svg')
+pio.write_image(fig, imagename, format='svg', width=1100, height=850)
 
 
