27145b261356f3be0da85dcbb0b53afcc8ce2839
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2021-05-06 11:38:44 -0400

    wonk

--- room_analysis.py
+++ room_analysis.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import plotly.figure_factory as ff
 import plotly.io as pio
+from plotly import graph_objects as go
+import plotly.express as px
 
 
 def last_step_auc_heatmap(csv_name):
@@ -36,7 +38,80 @@ def get_rolling_average(csv_name, bin_size=100):
     ordered_groups = df.groupby(['iteration'])
 
     for name, group in ordered_groups:
-        if int(name) % 100 == 0:
+        if int(name) % bin_size == 0:
             rolling_average.append(group['agent_auc'].mean())
 
     return rolling_average
+
+
+def auc_distrs_chunk(csv_name, bin_sizes=[100, 200, 300, 400, 500, 600, 1200, 2400, 2999]):
+    distrs = []
+    path = os.path.join(os.getcwd(), 'data', csv_name)
+    df = pd.read_csv(path)
+
+    ordered_groups = df.groupby(['iteration'])
+
+    i = 0
+    for name, group in ordered_groups:
+        if int(name) % bin_sizes[i] == 0:
+            distrs.append(group['agent_auc'].values)
+            i += 1
+
+    return distrs
+
+
+# x = get_rolling_average('p11_p21_p31.csv')
+# y = get_rolling_average('p12_p21_p31.csv')
+# z = get_rolling_average('p13_p21_p31.csv')
+# t = [(i*100) + 1 for i in range(30)]
+
+
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=t, y=x, mode='lines', name='p11_p21_p31' + ' min', marker_color='blue'))
+# fig.add_trace(go.Scatter(x=t, y=y, mode='lines', name='p12_p21_p31' + ' max', marker_color='green'))
+# fig.add_trace(go.Scatter(x=t, y=z, mode='lines', name='p13_p21_p31' + ' mean', marker_color='red'))
+# fig.update_layout(title='AUC Different Sink/Source Locations', font_family='arial', font_size=20)
+# fig.update_xaxes(title='Step')
+# fig.update_yaxes(title='AUC')
+# imagename = os.path.join(os.getcwd(), "data", 'P1_Variation.svg')
+# pio.write_image(fig, imagename, format='svg', width=1100, height=850)
+
+
+x = auc_distrs_chunk('p11_p21_p31.csv')
+y = auc_distrs_chunk('p12_p21_p31.csv')
+z = auc_distrs_chunk('p13_p21_p31.csv')
+
+df = pd.DataFrame()
+
+x1 = [item for sublist in x for item in sublist]
+y1 = [item for sublist in y for item in sublist]
+z1 = [item for sublist in z for item in sublist]
+x_group = [1 for val in x1]
+y_group = [2 for val in x1]
+z_group = [3 for val in x1]
+steps = []
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        steps.append(i + 1)
+
+bin_sizes = [100, 200, 300, 400, 500, 600, 1200, 2400, 2999]
+all_bins = []
+for i in range(len(bin_sizes)):
+    for j in range(len(x[0])):
+        all_bins.append(bin_sizes[i])
+
+df['val'] = x1 + y1 + z1
+df['step'] = steps + steps + steps
+df['sim_group'] = x_group + y_group + z_group
+df['bins'] = all_bins + all_bins + all_bins
+
+# TODO scale x axes
+
+
+fig = px.box(df, x="bins", y="val", color="sim_group")
+fig.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
+fig.update_xaxes(title='Step')
+fig.update_yaxes(title='Virus Intake')
+fig.show()
+
+
